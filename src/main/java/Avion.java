6677f9c23290dbe8b4fe29c3898772788e0538e8
daw103 <juancarloscarnerocortez@gmail.com>
2024-06-20 10:23:19 +0200

    Segundo Commit

--- src/main/java/Avion.java
+++ src/main/java/Avion.java
@@ -1,5 +1,7 @@
 public class Avion {
     public static void main(String[] args) {
 
+        System.out.println("Esta es la clase principal");
+
     }
 }
--- src/main/java/AvionMilitar.java
+++ src/main/java/AvionMilitar.java
@@ -1,2 +1,4 @@
 public class AvionMilitar extends Avion{
+
+
 }
